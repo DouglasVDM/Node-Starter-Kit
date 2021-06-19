9531b562d7b2370b735e5344eb8ca1b10d219dfb
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-06-19 22:49:47 +0200

    add two endpoints

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get("/chocolate", (req, res) => {
   res.send("Mmm Chocolate :0")
 })
 
+app.get("/node", (req, res) => {
+  res.send("Tennis Balls!")
+})
+
+app.get("/codeyourfuture", (req, res) => {
+  res.send("Is an awesome Community!")
+})
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
