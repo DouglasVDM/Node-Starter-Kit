e3a66a351374591bbe2fe21bbeab08e543fa925e
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-03 13:03:59 +0200

    get album by id

--- server.js
+++ server.js
@@ -2,29 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = 5000;
-
-const albumsData = [
-  {
-    albumId: "10",
-    artistName: "Beyoncé",
-    collectionName: "Lemonade",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
-    releaseDate: "2016-04-25T07:00:00Z",
-    primaryGenreName: "Pop",
-    url: "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-  {
-    albumId: "11",
-    artistName: "Beyoncé",
-    collectionName: "Dangerously In Love",
-    artworkUrl100:
-      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
-    releaseDate: "2003-06-24T07:00:00Z",
-    primaryGenreName: "Pop",
-    url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
-  },
-];
+const albumsData = require('./albumsData');
 
 app.get('/', (req, res) => {
   res.send("My name is Douglas");
@@ -32,6 +10,22 @@ app.get('/', (req, res) => {
 
 app.get('/album', (req, res) => {
   res.send(albumsData);
+});
+
+app.get('/album/:albumId', (req, res) => {
+  // console.log(req.params.albumId);
+  const { albumId } = req.params;
+  const match = albumsData.some(album => album.albumId === albumId);
+  console.log(match)
+  
+  if (match) {
+    const found = albumsData.find(album => album.albumId === albumId);
+    res.send(found)
+  } else {
+    res.status(404).send({
+      msg: `No album with the id of ${albumId}`
+    })
+  };
 });
 
 app.listen(PORT, () => {
